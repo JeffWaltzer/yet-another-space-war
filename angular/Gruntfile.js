87338b6c751cf52ea474b328650abc70447f2833
John Maxwell <jmax@jmaxhome.com>
2014-12-29 13:29:10 -0500

    Fixed incorrect path in grunt task for jshint.

--- angular/Gruntfile.js
+++ angular/Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
 
         jshint: {
-            all: ['source/**/*.js'],
+            all: ['src/**/*.js'],
             options: {
                 globals: {
                     _: false,
