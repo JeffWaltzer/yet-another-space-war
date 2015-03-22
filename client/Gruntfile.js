875f9a49f068835c49e502ec88744b583ad13243
John.Maxwell <jmax@jmaxhome.com>
2015-03-22 18:43:53 -0400

    More on release task (formerly known as deploy).

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -1,57 +1,51 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-            jshint: {
-                all: ['src/**/*.js'],
-                options: {
-                    globals: {
-                        _: false,
-                        $: false
-                    },
-                    browser: true,
-                    devel: true
-                }
-            },
+  grunt.initConfig({
+    jshint: {
+      all: ['src/**/*.js'],
+      options: {
+        globals: {
+          _: false,
+          $: false
+        },
+        browser: true,
+        devel: true
+      }
+    },
 
-            coffee: {
-                glob_to_multiple: {
-                    expand: true,
-                    flatten: true,
-                    cwd: "spec",
-                    src: ['*.coffee'],
-                    dest: 'spec/obj',
-                    ext: '.js'
-                }
-            },
+    coffee: {
+      glob_to_multiple: {
+        expand: true,
+        flatten: true,
+        cwd: "spec",
+        src: ['*.coffee'],
+        dest: 'spec/obj',
+        ext: '.js'
+      }
+    },
 
-            karma: {
-                unit: {
-                    configFile: 'karma.conf.js'
-                }
-            },
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
+    },
 
-            deploy_client: {
-              to: '../server/public',
-              files: [],
-            },
-        }
-    );
-    grunt.loadNpmTasks('grunt-contrib-coffee');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-karma');
+    copy: {
+      deploy: {
+        files: [
+          {expand: true, cwd: 'src', src: ['**'],     dest: '../server/public', filter: 'isFile'},
+          {expand: true,             src: ['lib/**'], dest: '../server/public', filter: 'isFile'},
+        ],
+      },
+    },
 
-    grunt.registerTask('default', ['coffee', 'jshint','karma']);
+    clean: {
+      deploy: ['../server/public'],
+    },
+  });
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-karma');
 
-    grunt.registerTask('deploy_client', function() {
-      grunt.config.requires('deploy_client.to');
-      grunt.config.requires('deploy_client.files');
-
-      var to= grunt.config.get('deploy_client.to');
-      var files= grunt.config.get('deploy_client.files');
-
-      grunt.file.delete(to);
-      files.forEach(function(file) {
-        var destination= to + '/' + file;
-        grunt.file.copy(file, destination);
-      });
-    });
+  grunt.registerTask('default', ['coffee', 'jshint','karma']);
 };
