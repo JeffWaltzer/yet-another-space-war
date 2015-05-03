4bca68c9ef6416d342807a09c02c975298296e48
John.Maxwell <jmax@jmaxhome.com>
2015-05-03 16:05:43 -0400

    Refactoring: moved coordinate transform functions out of ship.js and into new transform.js module.

--- server/src/ship.js
+++ server/src/ship.js
@@ -1,35 +1,5 @@
 var underscore= require('underscore');
-
-var concatenate_transforms= function(result, a, b) {
-  for (var i= 0; i < 3; i++)
-	  for (var j= 0; j < 3; j++) {
-	    result[i][j]= 0;
-	    for (var k= 0; k < 3; k++)
-		    result[i][j] += a[i][k]*b[k][j];
-	  }
-  return result;
-};
-
-var apply_transform= function(result, a, v) {
-  for (var i= 0; i < 3; i++)
-	  result[i]= a[i][0] * v[0] +
-		a[i][1] * v[1] +
-		a[i][2];
-
-  return result;
-};
-
-var make_translation= function(x_offset, y_offset) {
-  return [[1, 0, x_offset],
-    			[0, 1, y_offset],
-    			[0, 0, 1]];
-};
-
-var make_rotation= function(angle) {
-  return [[Math.cos(angle), -Math.sin(angle), 0],
-    		  [Math.sin(angle),  Math.cos(angle), 0],
-    		  [0, 0, 1]];
-};
+var transforms= require('./transform');
 
 exports.Ship= function(initial_state) {
   var self= this;
@@ -41,9 +11,9 @@ exports.Ship= function(initial_state) {
   self.location= initial_state.location || [0,0];
 
   self.outline= function() {
-    var translation_out=  make_translation(-self.location[0], -self.location[1]);
-    var translation_back= make_translation(self.location[0],   self.location[1]);
-    var rotation=         make_rotation(self.heading);
+    var translation_out=  transforms.make_translation(-self.location[0], -self.location[1]);
+    var translation_back= transforms.make_translation(self.location[0],   self.location[1]);
+    var rotation=         transforms.make_rotation(self.heading);
 
     var tmp= [[0, 0, 0],
     		      [0, 0, 0],
@@ -53,13 +23,13 @@ exports.Ship= function(initial_state) {
     				                  [0, 1, 0],
     				                  [0, 0, 1]];
 
-    concatenate_transforms(tmp, translation_back, rotation);
-	  concatenate_transforms(composite_transform, tmp, translation_out);
+    transforms.concatenate_transforms(tmp, translation_back, rotation);
+	  transforms.concatenate_transforms(composite_transform, tmp, translation_out);
 
     var returned_points= underscore.map(self.points,
     					                          function(p) {
     						                          var rv= [0, 0, 0];
-    						                          apply_transform(rv, composite_transform, p);
+    						                          transforms.apply_transform(rv, composite_transform, p);
 						                              return [rv[0]/rv[2], rv[1]/rv[2]];
     					                          });
 	  return returned_points;
