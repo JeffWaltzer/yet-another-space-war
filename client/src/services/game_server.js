45ebae371a6d425fed943d32920fd84fdf4e6768
John.Maxwell <jmax@jmaxhome.com>
2015-05-03 21:12:07 -0400

    Fixed inadvertant global variable in client game_server.js

--- client/src/services/game_server.js
+++ client/src/services/game_server.js
@@ -3,7 +3,7 @@ angular.module('YASW').factory('game_server', function() {
   var service= {};
 
   service.send= function(e) {
-    message= {command: e};
+    var message= {command: e};
     service.web_socket.send(JSON.stringify(message));
   };
 
