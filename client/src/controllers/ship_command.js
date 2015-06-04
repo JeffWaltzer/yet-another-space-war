440e5f635a9a51cd2e03c4c032ace069ebcf1f87
John Maxwell <jmax@jmaxhome.com>
2015-06-04 11:10:08 -0400

    Started cleaning up and genericizing client keyboard handling.

--- client/src/controllers/ship_command.js
+++ client/src/controllers/ship_command.js
@@ -1,9 +1,4 @@
 angular.module('YASW').controller('ShipCommandController', function($scope, game_server) {
-  var key_in_state= function(key, state) { return $scope[key + '_key'] === state; };
-  var rotation_keys_in_state= function(left, right) {
-    return key_in_state('left', left) && key_in_state('right', right);
-  };
-
   var KEY_LEFT_ARROW= 37;
   var KEY_RIGHT_ARROW= 39;
   var KEY_DOWN_ARROW= 40;
@@ -22,97 +17,83 @@ angular.module('YASW').controller('ShipCommandController', function($scope, game
   $scope.down_key= 'up';
   $scope.fire_key= 'up';
 
+  var key_handlers= {};
 
-  $scope.onKeyDown= function(e) {
-    switch (e.keyCode) {
-      case KEY_LEFT_ARROW:
-        on_left_arrow_down();
-        break;
-      case KEY_RIGHT_ARROW:
-        on_right_arrow_down();
-        break;
-      case KEY_DOWN_ARROW:
-        on_down_arrow_down();
-        break;
-      case KEY_SPACE:
-        on_fire_down();
-        break;
-    }
-  };
+  function add_simple_key_handler(keycode, key_name, down_event, up_event) {
+    var key_in_state= function(state) { return $scope[key_name + '_key'] === state; };
 
-  $scope.onKeyUp= function(e) {
-    switch (e.keyCode) {
-      case KEY_LEFT_ARROW:
-        on_left_arrow_up();
-        break;
-      case KEY_RIGHT_ARROW:
-        on_right_arrow_up();
-        break;
-      case KEY_DOWN_ARROW:
-        on_down_arrow_up();
-        break;
-      case KEY_SPACE:
-        on_fire_up();
-        break;
-    }
-  };
+    key_handlers[keycode]= {
+      up: function() {
+	if (key_in_state('down') && up_event !== null)
+	  game_server.send(up_event);
+	$scope[key_name + '_key']= 'up';
+      },
+      down: function() {
+	if (key_in_state('up') && down_event !== null)
+	  game_server.send(down_event);
+	$scope[key_name + '_key']= 'down';
+      }
+    };
+  }
 
-//--------------------------------------------------------------
+  function add_opposition_key_pair_handler(left_keycode, left_key_name, right_keycode, right_key_name,
+					   left_down_event, left_up_event, right_down_event, right_up_event) {
 
-  var on_fire_down= function() {
-    if (key_in_state('fire', 'up'))
-      game_server.send('fire');
-    $scope.fire_key= 'down';
-  };
+    var left_key_in_state= function(state) { return $scope[left_key_name + '_key'] === state; };
+    var right_key_in_state= function(state) { return $scope[right_key_name + '_key'] === state; };
+    var keys_in_state= function(left, right) { return left_key_in_state(left) && right_key_in_state(right); };
 
-  var on_fire_up= function() {
-    $scope.fire_key= 'up';
-  };
+    key_handlers[left_keycode]= {
+      up: function() {
+	if (keys_in_state('down', 'down'))
+	  game_server.send('rotate_right');
+	else if (keys_in_state('down', 'up'))
+	  game_server.send('rotate_stop');
+	$scope.left_key = 'up';
+      },
+      down: function() {
+	if (keys_in_state('up', 'up'))
+	  game_server.send('rotate_left');
+	else if (keys_in_state('up', 'down'))
+	  game_server.send('rotate_stop');
+	$scope.left_key='down';
+      }
+    };
 
-  var on_down_arrow_down = function() {
-    if (key_in_state('down', 'up'))
-      game_server.send('thrust_on');
-    $scope.down_key = 'down';
-  };
+    key_handlers[right_keycode]= {
+      up: function() {
+	if (keys_in_state('down', 'down'))
+	  game_server.send('rotate_left');
+	else if (keys_in_state('up', 'down'))
+	  game_server.send('rotate_stop');
+	$scope.right_key='up';
+      },
+      down: function() {
+	if (keys_in_state('up', 'up'))
+	  game_server.send('rotate_right');
+	else if (keys_in_state('down', 'up'))
+	  game_server.send('rotate_stop');
+	$scope.right_key = 'down';
+      }
+    };
+  }
 
-  var on_down_arrow_up = function() {
-    if (key_in_state('down', 'down'))
-      game_server.send('thrust_off');
-    $scope.down_key = 'up';
-  };
+  add_simple_key_handler(KEY_SPACE, 'fire', 'fire', null);
+  add_opposition_key_pair_handler(KEY_LEFT_ARROW, 'left', KEY_RIGHT_ARROW, 'right',
+				  'rotate_left', 'rotate_stop', 'rotate_right', 'rotate_stop');
 
+  function handler_for(key_code) {
+    var handler= key_handlers[key_code];
+    if (typeof(handler) === 'undefined')
+      handler= {
+	up: function() {},
+	down: function() {}
+      };
+    return handler;
+  }
 
-  var on_left_arrow_down= function() {
-    if (rotation_keys_in_state('up', 'up'))
-      game_server.send('rotate_left');
-    else if (rotation_keys_in_state('up', 'down'))
-      game_server.send('rotate_stop');
-    $scope.left_key='down';
-  };
-
-  var on_left_arrow_up = function() {
-    if (rotation_keys_in_state('down', 'down'))
-      game_server.send('rotate_right');
-    else if (rotation_keys_in_state('down', 'up'))
-      game_server.send('rotate_stop');
-    $scope.left_key = 'up';
-  };
-
-
-  var on_right_arrow_down=function () {
-    if (rotation_keys_in_state('up', 'up'))
-      game_server.send('rotate_right');
-    else if (rotation_keys_in_state('down', 'up'))
-      game_server.send('rotate_stop');
-    $scope.right_key = 'down';
-  };
-
-  var on_right_arrow_up = function() {
-    if (rotation_keys_in_state('down', 'down'))
-      game_server.send('rotate_left');
-    else if (rotation_keys_in_state('up', 'down'))
-      game_server.send('rotate_stop');
-    $scope.right_key='up';
-  };
+  $scope.onKeyDown= function(e) { handler_for(e.keyCode).down(e); };
+  $scope.onKeyUp= function(e) { handler_for(e.keyCode).up(e); };
 
 });
+  
