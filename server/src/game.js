4d4c0ecf6edbc5e2a8334973ecd9622ab9b8f535
John.Maxwell <jmax@jmaxhome.com>
2015-06-22 17:41:59 -0400

    Refactoring in game.js; split tick() into three methods and renamed several parameters for clarity.

--- server/src/game.js
+++ server/src/game.js
@@ -4,16 +4,15 @@ var bullet=require('./bullet');
 var vector=require('./vector');
 
 exports.Game=function(server) {
-   var self = this;
-   self.add_screen_object= function(new_ship) {
-      self.screen_objects.push(new_ship);
-   };
-
+  var self = this;
+  self.add_screen_object= function(new_screen_object) {
+    self.screen_objects.push(new_screen_object);
+    return new_screen_object;
+  };
 
   self.field_size = new vector.Vector([800,600]);
 
   self.add_ship = function(parameters) {
-
     var defaultState = {
       game: self,
       rotation: 0,
@@ -25,45 +24,48 @@ exports.Game=function(server) {
     if (parameters !== undefined)
       underscore.extend(defaultState ,parameters);
 
-    var new_ship = new ship.Ship(defaultState);
-
-    self.add_screen_object(new_ship);
-    return new_ship;
+    return self.add_screen_object(new ship.Ship(defaultState));
   };
 
   self.add_bullet=function(parameters){
-    var new_bullet = new bullet.Bullet(parameters);
-    self.add_screen_object( new_bullet);
-    return new_bullet;
+    return self.add_screen_object(new bullet.Bullet(parameters));
   };
 
+  function each_screen_object(callback_function) {
+    underscore.each(self.screen_objects, callback_function);
+  }
+
+  function update_screen_objects() {
+    each_screen_object(
+      function(screen_object) {
+        screen_object.update(
+          server.ship_rotation_rate,
+          server.tick_rate,
+          server.acceleration_rate);
+      });
+  }
+
+  function game_board() {
+    var outlines = {};
+    each_screen_object(
+      function(screen_object, id) {
+        outlines[id] = screen_object.outline();
+      });
+    return JSON.stringify(outlines);
+  }
+
+  function send_game_board(new_board) {
+    each_screen_object(
+      function(ship) { if (ship.socket) ship.socket.send(new_board); });
+  }
+
   self.tick= function() {
-    underscore.each(self.screen_objects,
-        function(ship) {
-           ship.update(
-             server.ship_rotation_rate,
-             server.tick_rate,
-             server.acceleration_rate);
-        });
+    update_screen_objects();
+    send_game_board(game_board());
+  };
 
-      var ship_outlines = {};
-      underscore.each(self.screen_objects,
-        function(ship,ship_id) {
-           ship_outlines[ship_id] = ship.outline();
-        });
+  if (server.tick_rate!==0)
+    setInterval(self.tick, 1000/server.tick_rate);
 
-      var game_board = JSON.stringify(ship_outlines);
-
-      underscore.each(self.screen_objects,
-        function(ship) {
-           if (ship.socket) {
-              ship.socket.send(game_board);
-           }
-        });
-   };
-
-   if (server.tick_rate!==0)
-     setInterval(self.tick, 1000/server.tick_rate);
-
-   self.screen_objects=[];
+  self.screen_objects=[];
 };
