4dc9edc5600278c9fe0f055c843d2d12e97c0dd1
John Maxwell <jmax@Kubunutu-15.10-Laptop>
2016-05-18 20:26:12 -0400

    Code cleanup in Bullet and Fragment.

--- server/src/bullet.js
+++ server/src/bullet.js
@@ -1,44 +1,36 @@
-var vector = require('./vector');
-var screen_object= require('./screen_object');
+var ScreenObject= require('./screen_object').ScreenObject;
 var util = require('util');
 
 exports.Bullet = function(initial_state) {
-  screen_object.ScreenObject.call(this, initial_state);
+  ScreenObject.call(this, initial_state);
 
   var self = this;
 
-  self.velocity = new vector.Vector([0, 0]);
-
   self.life_left = initial_state.life_left || 0;
-
-  if (initial_state.velocity) {
-    self.velocity = new vector.Vector(
-      initial_state.velocity);
-  }
-
-  self.ship=function(new_value) {
-    if(new_value) {
-      self._ship = new_value;
-    }
-    return self._ship;
-  };
-
-  self.is_bullet = function() {
-    return true;
-  };
 
   if (initial_state.ship) {
     self.ship( initial_state.ship);
   }
-
-  self.update= function(tick_rate, rotation_rate, acceleration_rate) {
-    self.life_left -= 1 / tick_rate;
-    exports.Bullet.super_.prototype.update.call(this, tick_rate);
-  };
-
-  self.live = function() {
-    return self.life_left > 0;
-  };
 };
 
-util.inherits(exports.Bullet, screen_object.ScreenObject);
+util.inherits(exports.Bullet, ScreenObject);
+
+exports.Bullet.prototype.ship= function(new_value) {
+    if(new_value) {
+        this._ship = new_value;
+    }
+    return this._ship;
+};
+
+exports.Bullet.prototype.is_bullet = function() {
+    return true;
+};
+
+exports.Bullet.prototype.update= function(tick_rate, rotation_rate, acceleration_rate) {
+    this.life_left -= 1 / tick_rate;
+    exports.Bullet.super_.prototype.update.call(this, tick_rate);
+};
+
+exports.Bullet.prototype.live = function() {
+    return this.life_left > 0;
+};
--- server/src/fragment.js
+++ server/src/fragment.js
@@ -10,31 +10,6 @@ exports.Fragment = function(initial_state) {
   // self.velocity = new vector.Vector([0, 0]);
 
   this.life_left = initial_state.life_left || 0;
-
-  // if (initial_state.velocity) {
-  //   self.velocity = new vector.Vector(
-  //     initial_state.velocity);
-  // }
-
-  // self.ship=function(new_value) {
-  //   if(new_value) {
-  //     self._ship = new_value;
-  //   }
-  //   return self._ship;
-  // };
-
-  // if (initial_state.ship) {
-  //   self.ship( initial_state.ship);
-  // }
-
-  // self.update= function(tick_rate, rotation_rate, acceleration_rate) {
-  //   self.life_left -= 1 / tick_rate;
-  //   exports.Fragment.super_.prototype.update.call(this, tick_rate);
-  // };
-
-  // self.live = function() {
-  //   return self.life_left > 0;
-  // };
 };
 
 util.inherits(exports.Fragment, screen_object.ScreenObject);
