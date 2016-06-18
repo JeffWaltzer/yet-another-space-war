5435028a8ec30ae2f5c0bf051b256b1b27801743
John Maxwell <jmax@jmaxhome.com>
2016-06-17 22:36:59 -0400

    Added method() method.

--- server/src/screen_object.js
+++ server/src/screen_object.js
@@ -15,126 +15,115 @@ var ScreenObject = function(initial_state) {
 };
 exports.ScreenObject= ScreenObject;
 
-meta.method(ScreenObject,
-	    'position',
-	    function(new_value) {
-		if (typeof new_value !== 'undefined') {
-		    this._position = new_value;
-		    this.update_outline();
-		}
-		return this._position;
-	    });
+ScreenObject.method= function(name, body) {
+    ScreenObject.prototype[name]= body;
+    return body;
+};
 
-meta.method(ScreenObject,
-	    'ignores_collisions',
-	    function() { return false; });
+ScreenObject.method('position',
+		    function(new_value) {
+			if (typeof new_value !== 'undefined') {
+			    this._position = new_value;
+			    this.update_outline();
+			}
+			return this._position;
+		    });
 
-meta.method(ScreenObject,
-	    'ship_to_game_transform',
-	    function() { return transforms.make_translation(this._position); });
+ScreenObject.method('ignores_collisions',
+		    function() { return false; });
 
-meta.method(ScreenObject,
-	    'generate_outline',
-	    function() {
-		var self = this;
+ScreenObject.method('ship_to_game_transform',
+		    function() { return transforms.make_translation(this._position); });
 
-		this.bounding_box = false;
+ScreenObject.method('generate_outline',
+		    function() {
+			var self = this;
 
-		var composite_transform = this.ship_to_game_transform();
-		var returned_points = underscore.map(this.points,
-						     function(p) {
-							 var rv = [0, 0, 0];
-							 transforms.apply_transform(rv, composite_transform, p);
-							 var x = (rv[0] / rv[2]);
-							 var y = (rv[1] / rv[2]);
-							 if (self.bounding_box) {
-							     if (x > self.bounding_box.right )  self.bounding_box.right = x;
-							     if (x < self.bounding_box.left )  self.bounding_box.left = x;
-							     if (y > self.bounding_box.top )  self.bounding_box.top = y;
-							     if (y < self.bounding_box.bottom )  self.bounding_box.bottom = y;
-							 } else {
-							     self.bounding_box = {};
-							     self.bounding_box.left = self.bounding_box.right = x;
-							     self.bounding_box.top = self.bounding_box.bottom = y;
-							 }
-							 return [x, y];
-						     });
-		return returned_points;
-	    });
+			this.bounding_box = false;
 
-meta.method(ScreenObject,
-	    'update_outline',
-	    function() { this.outline_cache = this.generate_outline(); });
+			var composite_transform = this.ship_to_game_transform();
+			var returned_points = underscore.map(this.points,
+							     function(p) {
+								 var rv = [0, 0, 0];
+								 transforms.apply_transform(rv, composite_transform, p);
+								 var x = (rv[0] / rv[2]);
+								 var y = (rv[1] / rv[2]);
+								 if (self.bounding_box) {
+								     if (x > self.bounding_box.right )  self.bounding_box.right = x;
+								     if (x < self.bounding_box.left )  self.bounding_box.left = x;
+								     if (y > self.bounding_box.top )  self.bounding_box.top = y;
+								     if (y < self.bounding_box.bottom )  self.bounding_box.bottom = y;
+								 } else {
+								     self.bounding_box = {};
+								     self.bounding_box.left = self.bounding_box.right = x;
+								     self.bounding_box.top = self.bounding_box.bottom = y;
+								 }
+								 return [x, y];
+							     });
+			return returned_points;
+		    });
 
-meta.method(ScreenObject,
-	    'is_bullet',
-	    function () { return false; });
+ScreenObject.method('update_outline',
+		    function() { this.outline_cache = this.generate_outline(); });
 
-meta.method(ScreenObject,
-	    'is_fragment',
-	    function () { return false; });
+ScreenObject.method('is_bullet',
+		    function () { return false; });
 
-meta.method(ScreenObject,
-	    'is_ship',
-	    function () { return false; });
+ScreenObject.method('is_fragment',
+		    function () { return false; });
 
-meta.method(ScreenObject,
-	    'outline',
-	    function() { return this.outline_cache; });
+ScreenObject.method('is_ship',
+		    function () { return false; });
 
-meta.method(ScreenObject,
-	    'update',
-	    function(tick_rate) {
-		this._position.add_to(this.velocity.divide(tick_rate));
-		this._position.clip_to(this.game.game_field.field_size());
-		this.update_outline();
-	    });
+ScreenObject.method('outline',
+		    function() { return this.outline_cache; });
 
-meta.method(ScreenObject,
-	    'live',
-	    function() { return true; });
+ScreenObject.method('update',
+		    function(tick_rate) {
+			this._position.add_to(this.velocity.divide(tick_rate));
+			this._position.clip_to(this.game.game_field.field_size());
+			this.update_outline();
+		    });
 
-meta.method(ScreenObject,
-	    'score',
-	    function() { return 0; });
+ScreenObject.method('live',
+		    function() { return true; });
 
-meta.method(ScreenObject,
-	    'lines',
-	    function () {
-		var result = [];
-		var transform_points = this.outline();
-		for(var i = 0; i< transform_points.length; i++) {
-		    result.push([
-			transform_points[i],
-			transform_points[(i+1) % transform_points.length]
-		    ]);
-		}
-		return result;
-	    });
+ScreenObject.method('score',
+		    function() { return 0; });
 
-meta.method(ScreenObject,
-	    'player',
-	    function(new_value) {
-		if (new_value !== undefined)
-		    this._player= new_value;
-		return this._player;
-	    });
+ScreenObject.method('lines',
+		    function () {
+			var result = [];
+			var transform_points = this.outline();
+			for(var i = 0; i< transform_points.length; i++) {
+			    result.push([
+				transform_points[i],
+				transform_points[(i+1) % transform_points.length]
+			    ]);
+			}
+			return result;
+		    });
 
-meta.method(ScreenObject,
-	    'make_game_piece',
-	    function() {
-		return {
-		    outline: this.outline(),
-		    id: this.id,
-		    score: this.score(),
-		    position: [
-			this.position().x(),
-			this.position().y()
-		    ]
-		};
-	    });
+ScreenObject.method('player',
+		    function(new_value) {
+			if (new_value !== undefined)
+			    this._player= new_value;
+			return this._player;
+		    });
 
-meta.method(ScreenObject,
-	    'explode',
-	    function() {});
+ScreenObject.method('make_game_piece',
+		    function() {
+			return {
+			    outline: this.outline(),
+			    id: this.id,
+			    score: this.score(),
+			    position: [
+				this.position().x(),
+				this.position().y()
+			    ]
+			};
+		    });
+
+ScreenObject.method('explode',
+		    function() {});
 
