a399cb30af5b16c0d7d6eaada68b452cd5cc8620
John Maxwell <jmax@jmaxhome.com>
2016-08-01 19:40:52 -0400

    Overhauled fragment shapes; much nicer now.

--- server/src/fragment_maker.js
+++ server/src/fragment_maker.js
@@ -1,19 +1,38 @@
 var underscore = require('underscore');
 
-
-var shape = [[5, 0], [1.24, 3.80], [-4.85, 3.53], [-2.43, -1.76], [1.85, -2.85]];
-
+function make_fragment(n_sides, base_size) {
+    var fragment= [];
+    for (var i= 0; i < n_sides; i++) {
+	var twiddle= 1 + 0.8 * Math.random() - 0.4;
+	var angle= (2*Math.PI/n_sides) * i;
+	var size= base_size*twiddle;
+	fragment.push([size*Math.cos(angle),
+		       size*Math.sin(angle)]);
+    }
+    return fragment;
+}
 
 var fragmentShapes = [
-  underscore.map(shape, function (point) {
-    return [point[0] * 1, point[1] * 1];
-  }),
-  underscore.map(shape, function (point) {
-    return [point[0] * 3, point[1] * 2];
-  }),
-  underscore.map(shape, function (point) {
-    return [point[0] * 1, point[1] * 4];
-  })
+  make_fragment(Math.floor(10.5 + Math.random()),
+		5*Math.random() + 5),
+  make_fragment(Math.floor(11.5 + Math.random()),
+		5*Math.random() + 5),
+  make_fragment(Math.floor(12.5 + Math.random()),
+		5*Math.random() + 5),
+  make_fragment(Math.floor(13.5 + Math.random()),
+		5*Math.random() + 5),
+  make_fragment(Math.floor(14.5 + Math.random()),
+		5*Math.random() + 5),
+  make_fragment(Math.floor(15.5 + Math.random()),
+		5*Math.random() + 5),
+  make_fragment(Math.floor(16.5 + Math.random()),
+		5*Math.random() + 5),
+  make_fragment(Math.floor(17.5 + Math.random()),
+		5*Math.random() + 5),
+  make_fragment(Math.floor(18.5 + Math.random()),
+		5*Math.random() + 5),
+  make_fragment(Math.floor(19.5 + Math.random()),
+		5*Math.random() + 5),
 ];
 
 exports.fragment_shapes = fragmentShapes;
