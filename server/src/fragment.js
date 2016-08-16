50039858b4f4cb4971df12ec32d98f3f32377393
John Maxwell <jmax@jmaxhome.com>
2016-08-16 08:24:31 -0400

    Fragment to consistent style.

--- server/src/fragment.js
+++ server/src/fragment.js
@@ -3,22 +3,24 @@ var screen_object= require('./screen_object');
 var MortalObject= require('./mortal_object');
 var util = require('util');
 
-exports.Fragment = function(initial_state) {
+function Fragment(initial_state) {
   initial_state.points= initial_state.points || [[-8.33, -5], [8.33, -5], [0, 5]];
   screen_object.ScreenObject.call(this, initial_state);
 
   this.life_left = initial_state.life_left || 3;
-};
+}
 
-util.inherits(exports.Fragment, screen_object.ScreenObject);
+util.inherits(Fragment, screen_object.ScreenObject);
 
-exports.Fragment.prototype.is_fragment = function() {
+Fragment.prototype.is_fragment = function() {
   return true;
 };
 
-exports.Fragment.prototype.ignores_collisions= function() {
+Fragment.prototype.ignores_collisions= function() {
     return true;
 };
 
-exports.Fragment.prototype.update= MortalObject.update;
-exports.Fragment.prototype.live = MortalObject.live;
+Fragment.prototype.update= MortalObject.update;
+Fragment.prototype.live = MortalObject.live;
+
+exports.Fragment = Fragment;
