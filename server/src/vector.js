5d5cc24b72cdf6542fe7e76a2bf50a53a0be0737
John Maxwell <jmax@jmaxhome.com>
2016-08-18 23:38:53 -0400

    More un-selfing

--- server/src/vector.js
+++ server/src/vector.js
@@ -32,37 +32,39 @@ function Vector(coordinates) {
 	throw("invalid Object for new Vector");
       }
     }
-
-    var make_coordinate_accessor= function(i) {
-	return function(new_value) {
-	    if (typeof new_value !== 'undefined')
-		self.coordinates[i]= new_value;
-	    return self.coordinates[i];
-	};
-    };
-
-    self.x= make_coordinate_accessor(0);
-    self.y= make_coordinate_accessor(1);
-
-    self.add_to= function(v) {
-	self.x(self.x() + v.x());
-	self.y(self.y() + v.y());
-	return self;
-    };
-
-    self.clip_to= function(v) {
-	self.x(self.x() % v.x());
-	if (self.x() < 0) self.x(self.x() + v.x());
-
-	self.y(self.y() % v.y());
-	if (self.y() < 0) self.y(self.y() + v.y());
-    };
-
-    self.divide= function(s) {
-	return new Vector([self.x() / s,
-			   self.y() / s]);
-    };
 }
+
+Vector.prototype.make_coordinate_accessor= function(i) {
+    return function(new_value) {
+	if (typeof new_value !== 'undefined')
+	    this.coordinates[i]= new_value;
+	return this.coordinates[i];
+    };
+};
+
+Vector.prototype.x= Vector.prototype.make_coordinate_accessor(0);
+Vector.prototype.y= Vector.prototype.make_coordinate_accessor(1);
+
+Vector.prototype.add_to= function(v) {
+    this.x(this.x() + v.x());
+    this.y(this.y() + v.y());
+    return this;
+};
+
+Vector.prototype.clip_to= function(v) {
+    this.x(this.x() % v.x());
+    if (this.x() < 0)
+	this.x(this.x() + v.x());
+
+    this.y(this.y() % v.y());
+    if (this.y() < 0)
+	this.y(this.y() + v.y());
+};
+
+Vector.prototype.divide= function(s) {
+    return new Vector([this.x() / s,
+		       this.y() / s]);
+};
 
 exports.Vector= Vector;
 
