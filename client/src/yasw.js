4e4385b4834565369035a18284ac1d187f90ebd3
John Maxwell <jmax@jmaxhome.com>
2016-11-11 15:02:43 -0500

    Moved score display inside of screen-object
    And deleted score directive and references to it.

--- client/src/yasw.js
+++ client/src/yasw.js
@@ -10,24 +10,21 @@ angular.module('YASW', [])
     return {
       restrict: 'E',
       replace: true,
-      template: '<polygon' +
-        ' ng-attr-points="{{screen_object.polygon_string}}"' +
-        ' ng-attr-stroke="{{screen_object.color}}"' +
-        ' stroke-width="2"' +
-        ' fill="none"' + 
-        '/>'
-    };
-  })
-  .directive('score', function() {
-    return {
-      restrict: 'E',
-      replace: true,
-      template: '<text' +
-        ' ng-if="screen_object.score"' +
-        ' ng-attr-x="{{screen_object.position[0]}}"' +
-        ' ng-attr-y="{{screen_object.position[1] - 20}}"' +
-        ' ng-attr-fill="{{screen_object.color}}">' +
-        '{{screen_object.score}}' +
-        '</text>'
+      template: '' +
+        '<g>' +
+        '  <polygon' +
+        '   ng-attr-points="{{screen_object.polygon_string}}"' +
+        '   ng-attr-stroke="{{screen_object.color}}"' +
+        '   stroke-width="2"' +
+        '   fill="none"' +
+        '  />' +
+        '  <text' +
+        '   ng-if="screen_object.score"' +
+        '   ng-attr-x="{{screen_object.position[0]}}"' +
+        '   ng-attr-y="{{screen_object.position[1] - 20}}"' +
+        '   ng-attr-fill="{{screen_object.color}}">' +
+        '  {{screen_object.score}}' +
+        '  </text>' +
+        '</g>'
     };
   });
