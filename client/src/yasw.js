2072761316a0287e6010f377ccb1113a0e5c853b
John Maxwell <jmax@jmaxhome.com>
2016-11-11 13:39:01 -0500

    Added screen-object directive.

--- client/src/yasw.js
+++ client/src/yasw.js
@@ -5,4 +5,15 @@ angular.module('YASW', [])
         element[0].focus();
       }
     };
+  })
+  .directive('screenObject', function() {
+    return {
+      replace: true,
+      template: '<polygon' +
+        ' ng-attr-points="{{screen_object.polygon_string}}"' +
+        ' ng-attr-stroke="{{screen_object.color}}"' +
+        ' stroke-width="2"' +
+        ' fill="none"' + 
+        '/>'
+    };
   });
