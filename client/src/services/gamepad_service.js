0a5d79655c08b5539ae50f48de82afe072e95384
John Maxwell <jmax@Kubunutu-15.10-Laptop>
2017-01-09 20:45:43 -0500

    Refactoring of gamepad button bindings
    Changed button bindings from constants to a hash, and made gamepad
    generation depend on button bindings size.

--- client/src/services/gamepad_service.js
+++ client/src/services/gamepad_service.js
@@ -4,18 +4,26 @@ angular.module('YASW').factory('gamepad_service', [
 
     var service = {};
 
-    var fire_button_index = 0;
-    var thrust_button_index = 1;
+    var button_bindings= {
+      fire: 0,
+      thrust: 5,
+    };
+
+    var number_of_buttons= function() {
+      return _(_(button_bindings).values()).max() + 1;
+    };
 
     function YaswGamepad(dom_gamepad) {
-      var _dom_gamepad_ = dom_gamepad || {buttons: [
-          {pressed: false},
-          {pressed: false},
-        ]};
+      var _dom_gamepad_ = dom_gamepad || 
+          {
+            buttons: _(Array(number_of_buttons())).map(function() {
+              return {pressed: false};
+            })
+          };
 
       this.fire = function (new_value) {
 
-        var fire_button = _dom_gamepad_.buttons[fire_button_index];
+        var fire_button = _dom_gamepad_.buttons[button_bindings.fire];
 
         if (new_value!==undefined)
           fire_button.pressed = new_value;
@@ -24,7 +32,7 @@ angular.module('YASW').factory('gamepad_service', [
       };
 
       this.thrust = function (new_value) {
-        var thrust_button = _dom_gamepad_.buttons[thrust_button_index];
+        var thrust_button = _dom_gamepad_.buttons[button_bindings.thrust];
 
         if (new_value!==undefined)
           thrust_button.pressed = new_value;
@@ -36,10 +44,7 @@ angular.module('YASW').factory('gamepad_service', [
     service.YaswGamepad=YaswGamepad;
 
 
-    service.last_gamepad = new YaswGamepad({buttons: [
-      {pressed: false},
-      {pressed: false},
-    ]});
+    service.last_gamepad = new YaswGamepad();
 
     service.interpret_command = function (gamepad) {
 
